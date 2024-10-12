2e1f9f4a7b5af62e9d31b8d90ce869dfe7a1c96f
SchiavonNathan <Nathanmestrepk@gmail.com>
2024-10-12 18:14:14 -0300

    Update Bootstrap

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,19 @@ import path from "path";
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "/views"));
+app.set('img', path.join(__dirname, "/img"));
 
 app.get('/', function (request:Request, response: Response) {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=50")
         .then(function (res) {
             return res.json();
         })
         .then(function (data) {
             response.render("index", data);
+            
         });
 });
+
 
 app.get('/pokemon/:name', function (request:Request, response: Response) {
     const nomePoke = request.params.name;
@@ -27,7 +30,11 @@ app.get('/pokemon/:name', function (request:Request, response: Response) {
         });
 });
 
+app.get('/img', function (request:Request, response: Response) {
+            response.render("img");
+});
+
 
 app.listen(3000, function () {
-    console.log("Server is running");
+    console.log("Server is running in port 3000");
 })
